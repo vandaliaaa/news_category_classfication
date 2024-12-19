2e08bb7133ee4f8492c83a9f2396ec92b624dd58
scolpig <scolpig@gmail.com>
2024-12-19 10:35:19 +0900

    crawling

--- job01_crawling_headline.py
+++ job01_crawling_headline.py
@@ -15,7 +15,7 @@ df_titles = pd.DataFrame()
 
 
 #모든 뉴스 헤드라인 카테고리 가져옴
-for i in range(6):
+for i in range(2):
     #주소설정
     #주소는 맨뒤에 100~106까지 반복됨
     url = 'https://news.naver.com/section/10{}'.format(i)
@@ -54,7 +54,7 @@ print(df_titles['category'].value_counts())
 
 # CSV 파일로 저장 (파일명: naver_headline_news_YYYYMMDD.csv)
 #datetime.datetime.now() :현제 시간을 알려줌
-df_titles.to_csv('./crawlind_data/naver_headline_news_{}.csv'.format(
+df_titles.to_csv('./crawlind_data/naver_headline_news_0_1_{}.csv'.format(
     datetime.datetime.now().strftime('%Y%m%d')), index=False)
 
 
