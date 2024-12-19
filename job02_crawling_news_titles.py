aad4fc1f9e7fa69b2ae76b7969b4fa2fc408de0f
KGW0128 <96161768+KGW0128@users.noreply.github.com>
2024-12-19 11:56:33 +0900

    선언

--- job02_crawling_news_titles.py
+++ job02_crawling_news_titles.py
@@ -1,1 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from setuptools.package_index import user_agent
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+import pandas as pd
+import re
+import time
+import datetime
+
+options = ChromeOptions()
+user_agent = ''
